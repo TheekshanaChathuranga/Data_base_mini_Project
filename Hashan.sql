631c41114a4ed8f3d8c503e824fd0ad1fea58545
Hashan Madhuranga Dissanayaka <141218317+HashanMadhu@users.noreply.github.com>
2023-10-25 01:19:22 +0530

    Create table(C,S)
    Create the Course and Students tables

---
+++ Hashan.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Course(
+    Course_id VARCHAR(10),
+    Department_id VARCHAR(10),
+    C_name VARCHAR(30),
+    Lecture_in_charge VARCHAR(30),
+    Theory_OR_Practical VARCHAR(10),
+    CONSTRAINT Course PRIMARY KEY (Course_id,Department_id)
+    );
+
+    CREATE TABLE Student(
+    Student_id VARCHAR(10),
+    Name VARCHAR(30),
+    Contact_no VARCHAR(11),
+    Admin_id VARCHAR(10),
+    Officer_id VARCHAR(10),
+    Lecture_id VARCHAR(10),
+    Dean_id VARCHAR(10),
+    Course_id VARCHAR(10),
+    PRIMARY KEY (Student_id)
+    );
