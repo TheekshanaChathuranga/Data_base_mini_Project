27c82be8e99293ce4416158fc47d09291b31528c
Theekshan Chathuranga <theekshanacnb@gmail.com>
2023-10-28 14:19:02 +0530

    UPDATE

--- Chathuranga.sql
+++ Chathuranga.sql
@@ -33,3 +33,15 @@ CREATE TABLE Lecture
      Contact_no VARCHAR(11),
      PRIMARY KEY(Lecture_id)
      );
+
+
+INSERT INTO Course
+VALUES
+('ICT1212','ICT001','Database Management Systems','Mr.P.H.P.Nuwan Laksiri','Theory'),
+('ICT1222','ICT001','Database Management Systems Practicum','Mr.P.H.P.Nuwan Laksiri','Practical'),
+('ICT1233','ICT001','Server-Side Web Develop','Ms.Rumeshika Wickrama Arachchi','Theory'),
+('ICT1242','ICT001','Computer Architecture','Prof. Subash Jayasinghe','Theory'),
+('ICT1253','ICT001','Computer Networks','Ms.Chanduni Gamage','Practical'),
+('TCS1212','MD004','Fundamentals of Managemente','Ms.Nilamthi Adikaram','Practical'),
+('TMS1233','ET001','Discrete Mathematics','Mr.Harshana Senavirathna ','Theory'),
+('ENG1212','MD004','English II','Ms.Hiranya Amarasinghe ','Theory');
