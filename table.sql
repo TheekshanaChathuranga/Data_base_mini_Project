42c30d785e6d431156fdb5e4b3f707cb6029d61c
Anuhas Rajapaksha <53539181+INFIN17Y@users.noreply.github.com>
2023-10-29 12:08:58 +0530

    Update insert data

--- table.sql
+++ table.sql
@@ -2,34 +2,6 @@ create database LMS;
 
 use lms;
 
-create table Technical_officer(
-    Officer_id varchar(10),
-    Contact_no varchar(11),
-    Staff_id varchar(10),
-    primary key(Officer_id),
-    foreign key(Staff_id) references Staff(Staff_id)
-);
-
-create table Department(
-    Department_id varchar(10),
-    Name varchar(30),
-    primary key(Department_id)
-);
-
-create table Attendence(
-    Student_id varchar(10),
-    Course_id varchar(10),
-    Attendence integer(3)
-    foreign key(Student_id) references Student(Student_id),
-    foreign key(Course_id) references Course(Course_id)
-);
-
-create table Mark(
-    Student_id varchar(10),
-    Course_id varchar(10),
-    foreign key(Student_id) references Student(Student_id),
-    foreign key(Course_id) references Course(Course_id)
-);
 
 create table Quiz_mark(
     Student_id varchar(10),
