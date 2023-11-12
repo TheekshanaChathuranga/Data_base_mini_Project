c0c78c49d4966e35b865be606b0cfa9ce31ea9f0
Theekshan Chathuranga <theekshanacnb@gmail.com>
2023-11-12 22:57:53 +0530

    `create

--- Procedure.sql
+++ Procedure.sql
@@ -17,16 +17,26 @@ CALL  CheckStudentmidemarks('TG/2021/1011');
 
 CREATE PROCEDURE CalculateAdjustedAssessmentMarks(IN stuID VARCHAR(20))
 BEGIN
-    CREATE OR REPLACE VIEW StudentAssessment_Marks AS
-    SELECT DISTINCT e.Student_id, e.Course_id, 
-        (e.Assessments * 0.05) AS Adjusted_Assessment_Mark
-    FROM exam_marks e
-    INNER JOIN student s ON e.Student_id = s.Student_id
-    where Student_id=stuID;
+   SELECT DISTINCT * FROM StudentAssessment_Marks where Student_id=stuID;
 END //
+
 
 CALL CalculateAdjustedAssessmentMarks('TG/2021/1011') ;
 
 
  
 
+ CREATE PROCEDURE courseGPA (IN stuID VARCHAR(20))
+ BEGIN
+SELECT * FROM  Student_GradePoint_For_Years WHERE Student_id=stuID;
+END//
+
+CALL courseGPA('TG/2021/1011') ;
+
+
+CREATE PROCEDURE Final_GPA (IN stuID VARCHAR(20))
+ BEGIN
+SELECT DISTINCT Student_id,Grade_Point FROM  Student_GradePoint_For_Years WHERE Student_id=stuID;
+END//
+
+CALL Final_GPA('TG/2021/1011') ;
