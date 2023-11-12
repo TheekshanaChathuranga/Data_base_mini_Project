9d23051eace0e0a7b8e0d63b1000dabed20b354e
Theekshan Chathuranga <theekshanacnb@gmail.com>
2023-11-12 20:10:36 +0530

    Insert new PROCEDURE

--- Procedure.sql
+++ Procedure.sql
@@ -1,6 +1,32 @@
+delimiter //
+delimiter ;
+
+
+-- Mid mark check procedure
+
 CREATE PROCEDURE CheckStudentmidemarks (IN stuID VARCHAR(20))
 BEGIN
-SELECT * FROM StudentMidterm_Marks where Student_id=stuID;
-END;
+SELECT DISTINCT * FROM StudentMidterm_Marks where Student_id=stuID;
+END //
 
+CALL  CheckStudentmidemarks('TG/2021/1011');
+
+
+
+
+
+CREATE PROCEDURE CalculateAdjustedAssessmentMarks(IN stuID VARCHAR(20))
+BEGIN
+    CREATE OR REPLACE VIEW StudentAssessment_Marks AS
+    SELECT DISTINCT e.Student_id, e.Course_id, 
+        (e.Assessments * 0.05) AS Adjusted_Assessment_Mark
+    FROM exam_marks e
+    INNER JOIN student s ON e.Student_id = s.Student_id
+    where Student_id=stuID;
+END //
+
+CALL CalculateAdjustedAssessmentMarks('TG/2021/1011') ;
+
+
+ 
 
